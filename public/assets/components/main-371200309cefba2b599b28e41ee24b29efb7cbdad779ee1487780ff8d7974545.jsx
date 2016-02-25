84952db5b022d9efb2e02bf67b9aa8e602b809f3
jundalisay <jundalisay@yahoo.com>
2016-02-25 12:25:12 +0700

    precompile

---
+++ public/assets/components/main-371200309cefba2b599b28e41ee24b29efb7cbdad779ee1487780ff8d7974545.jsx
@@ -0,0 +1,11 @@
+var Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <Header title={'Blog'} />
+        <h3>Main.jsx</h3>
+        <Body />
+      </div>
+    )
+  }
+});
